02824a69e79f9acaa3f8c8e11464b1c747bd4437
Vasilis Nakos <vnakos@customedialabs.com>
2015-09-08 18:40:02 +0300

    initial commit

---
+++ js/global.js
@@ -0,0 +1,9 @@
+'use strict';
+$(window).load(function (){
+    $('.pods-container').equalHeights();
+
+
+    //setTimeout(function (){
+    //    $('.pods-container').equalHeights('destroy');
+    //},1000);
+});
